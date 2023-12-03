e0eebecf9ee56d413c5b26403e46ec30f26e4c35
sunly <sunjingxiang1995@gmail.com>
2023-12-04 00:53:53 +0800

    feat: vue-repl

---
+++ playground/src/App.vue
@@ -0,0 +1,32 @@
+<!--
+ * @Description: repl
+ * @Author: Sunly
+ * @Date: 2023-12-03 15:34:19
+-->
+<script lang="ts" setup>
+import { Repl, ReplStore } from "@vue/repl";
+import Monaco from "@vue/repl/monaco-editor";
+import { reactive } from "vue";
+
+const store = new ReplStore({
+  showOutput: true,
+  outputMode: "preview",
+});
+
+const state = reactive({
+  showCompileOutput: true,
+  showImportMap: true,
+  previewOptions: { headHTML: "" },
+});
+</script>
+
+<template>
+  <Repl
+    :store="store"
+    :editor="Monaco"
+    :show-compile-output="state.showCompileOutput"
+    :show-import-map="state.showImportMap"
+    :previewOptions="state.previewOptions"
+    :clear-console="false"
+  ></Repl>
+</template>
